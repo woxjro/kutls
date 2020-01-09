81b940e56857c9aa536cb1413ac1fcef5af2d40b
YU <yoshi_nn_ihsoy@icloud.com>
2020-01-09 16:46:47 +0900

    KumirenとKumiren2membersにリレーションを追加

--- app/Admin/Controllers/KumirenController.php
+++ app/Admin/Controllers/KumirenController.php
@@ -31,6 +31,10 @@ class KumirenController extends AdminController
         $grid->column('name', __('Name'));
         $grid->column('created_at', __('Created at'));
         $grid->column('updated_at', __('Updated at'));
+        $grid->Kumiren2members('Kumiren to Members')->display(function ($kumiren2members) {
+            $count = count($kumiren2members);
+            return "<span class='label label-warning'>{$count}</span>";
+        });
 
         return $grid;
     }
@@ -49,6 +53,11 @@ class KumirenController extends AdminController
         $show->field('name', __('Name'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
+        $show->Kumiren2members('Kumiren to Members', function ($kumiren2members) {
+            $kumiren2members->resource('/admin/auth/kumiren2members');
+            $kumiren2members->id();
+            $kumiren2members->member_id();
+        });
 
         return $show;
     }
