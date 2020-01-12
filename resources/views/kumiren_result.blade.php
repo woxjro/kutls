4e0d7465c976ce501c795821b85d36cb273ed8d6
YU <yoshi_nn_ihsoy@icloud.com>
2020-01-12 09:42:25 +0900

    リザルト画面にチームを表示

--- resources/views/kumiren_result.blade.php
+++ resources/views/kumiren_result.blade.php
@@ -34,6 +34,43 @@
                         <p>・球出し：<?php echo ($fiscal_year - $feeds[4]->enrollment_year + 1)."回生：".$sex[$feeds[4]->sex]."：".$feeds[4]->last_name;?></p>
                         <p>・球出し：<?php echo ($fiscal_year - $feeds[5]->enrollment_year + 1)."回生：".$sex[$feeds[5]->sex]."：".$feeds[5]->last_name;?></p>
 
+
+
+                        @foreach($kumirenmembersinfo as $kumirenmemberinfo)
+                            @if($kumirenmemberinfo["team"]=="A")
+                                <p><?php echo "・".$kumirenmemberinfo["team"]."：".$kumirenmemberinfo["role"]."：".($fiscal_year - $kumirenmemberinfo["enrollment_year"] + 1)."回生：".$sex[$kumirenmemberinfo["sex"]]."：".$kumirenmemberinfo["last_name"];?></p>
+                            @endif
+                        @endforeach
+                        <br />
+                        @foreach($kumirenmembersinfo as $kumirenmemberinfo)
+                            @if($kumirenmemberinfo["team"]=="B")
+                                <p><?php echo "・".$kumirenmemberinfo["team"]."：".$kumirenmemberinfo["role"]."：".($fiscal_year - $kumirenmemberinfo["enrollment_year"] + 1)."回生：".$sex[$kumirenmemberinfo["sex"]]."：".$kumirenmemberinfo["last_name"];?></p>
+                            @endif
+                        @endforeach
+                        <br />
+                        @foreach($kumirenmembersinfo as $kumirenmemberinfo)
+                            @if($kumirenmemberinfo["team"]=="C")
+                                <p><?php echo "・".$kumirenmemberinfo["team"]."：".$kumirenmemberinfo["role"]."：".($fiscal_year - $kumirenmemberinfo["enrollment_year"] + 1)."回生：".$sex[$kumirenmemberinfo["sex"]]."：".$kumirenmemberinfo["last_name"];?></p>
+                            @endif
+                        @endforeach
+                        <br />
+                        @foreach($kumirenmembersinfo as $kumirenmemberinfo)
+                            @if($kumirenmemberinfo["team"]=="D")
+                                <p><?php echo "・".$kumirenmemberinfo["team"]."：".$kumirenmemberinfo["role"]."：".($fiscal_year - $kumirenmemberinfo["enrollment_year"] + 1)."回生：".$sex[$kumirenmemberinfo["sex"]]."：".$kumirenmemberinfo["last_name"];?></p>
+                            @endif
+                        @endforeach
+                        <br />
+                        @foreach($kumirenmembersinfo as $kumirenmemberinfo)
+                            @if($kumirenmemberinfo["team"]=="E")
+                                <p><?php echo "・".$kumirenmemberinfo["team"]."：".$kumirenmemberinfo["role"]."：".($fiscal_year - $kumirenmemberinfo["enrollment_year"] + 1)."回生：".$sex[$kumirenmemberinfo["sex"]]."：".$kumirenmemberinfo["last_name"];?></p>
+                            @endif
+                        @endforeach
+                        <br />
+                        @foreach($kumirenmembersinfo as $kumirenmemberinfo)
+                            @if($kumirenmemberinfo["team"]=="F")
+                                <p><?php echo "・".$kumirenmemberinfo["team"]."：".$kumirenmemberinfo["role"]."：".($fiscal_year - $kumirenmemberinfo["enrollment_year"] + 1)."回生：".$sex[$kumirenmemberinfo["sex"]]."：".$kumirenmemberinfo["last_name"];?></p>
+                            @endif
+                        @endforeach
                     </div>
                     <div class="form-group">
                         <div class="col-sm-offset-3 col-sm-6">
