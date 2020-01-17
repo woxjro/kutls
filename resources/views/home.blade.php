fc68be62ad07ebb34469797bb592b05893ac0ba5
YU-NN <yoshi_nn_ihsoy@icloud.com>
2020-01-17 21:08:03 +0900

    ログイン後の画面にTopへ戻るボタンを追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">なにかの一覧とか</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,9 +14,15 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <p>
+                        ログインしてる。。。
+                    </p>
 
-
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-6">
+                              <button onclick="location.href='{{url('/')}}'" class="btn btn-default"><i class="fa fa-btn fa-plus"></i>Topへ戻る</button>
+                        </div>
+                    </div>
 
                 </div>
             </div>
--- resources/views/kumiren_result.blade.php
+++ resources/views/kumiren_result.blade.php
@@ -102,7 +102,7 @@
                     </div>
                     <div class="form-group">
                         <div class="col-sm-offset-3 col-sm-6">
-                              <button onclick="location.href='{{url('/')}}'" class="btn btn-default"><i class="fa fa-btn fa-plus"></i>Homeへ戻る</button>
+                              <button onclick="location.href='{{url('/')}}'" class="btn btn-default"><i class="fa fa-btn fa-plus"></i>Topへ戻る</button>
                         </div>
                     </div>
 
