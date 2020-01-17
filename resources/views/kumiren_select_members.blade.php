12d3d240c7da081d3d85cdd962b35052211a2a2e
YU-NN <yoshi_nn_ihsoy@icloud.com>
2020-01-18 01:04:51 +0900

    アコーディオンのアイコンをクリック後に回転

--- resources/views/kumiren_select_members.blade.php
+++ resources/views/kumiren_select_members.blade.php
@@ -28,9 +28,8 @@
                         <form method="post" action="{{url('/kumiren/select_members/select_staffs')}}">
                             @csrf
                             <div class="select-members-part">
-
-                                <label  class="grade-male-label" for="cp_menu_bar1">6回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar1" class="accordion" />
+                                <label  class="grade-male-label" for="cp_menu_bar1">6回生</label>
                                 <ul id="link1">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 6)
@@ -46,9 +45,8 @@
                                     @endforeach
                                 </ul>
 
-
-                                <label class="grade-male-label" for="cp_menu_bar2">5回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar2" class="accordion" />
+                                <label class="grade-male-label" for="cp_menu_bar2">5回生</label>
                                 <ul id="link2">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 5)
@@ -64,9 +62,8 @@
                                     @endforeach
                                 </ul>
 
-
-                                <label class="grade-male-label"for="cp_menu_bar3">4回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar3" class="accordion" />
+                                <label class="grade-male-label"for="cp_menu_bar3">4回生</label>
                                 <ul id="link3">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 4)
@@ -82,9 +79,8 @@
                                     @endforeach
                                 </ul>
 
-
-                                <label class="grade-male-label"for="cp_menu_bar4">3回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar4" class="accordion" />
+                                <label class="grade-male-label"for="cp_menu_bar4">3回生</label>
                                 <ul id="link4">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 3)
@@ -100,9 +96,8 @@
                                     @endforeach
                                 </ul>
 
-
-                                <label class="grade-male-label" for="cp_menu_bar5">2回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar5" class="accordion" />
+                                <label class="grade-male-label" for="cp_menu_bar5">2回生</label>
                                 <ul id="link5">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 2)
@@ -118,9 +113,8 @@
                                     @endforeach
                                 </ul>
 
-
-                                <label class="grade-male-label" for="cp_menu_bar6">1回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar6" class="accordion" />
+                                <label class="grade-male-label" for="cp_menu_bar6">1回生</label>
                                 <ul id="link6">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 1)
@@ -138,8 +132,8 @@
 
                             </div>
                             <div class="select-members-part">
-                                <label  class="grade-female-label" for="cp_menu_bar7">6回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar7" class="accordion" />
+                                <label  class="grade-female-label" for="cp_menu_bar7">6回生</label>
                                 <ul id="link7">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 6)
@@ -155,8 +149,8 @@
                                     @endforeach
                                 </ul>
 
-                                <label  class="grade-female-label" for="cp_menu_bar8">5回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar8" class="accordion" />
+                                <label  class="grade-female-label" for="cp_menu_bar8">5回生</label>
                                 <ul id="link8">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 5)
@@ -172,8 +166,8 @@
                                     @endforeach
                                 </ul>
 
-                                <label  class="grade-female-label" for="cp_menu_bar9">4回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar9" class="accordion" />
+                                <label  class="grade-female-label" for="cp_menu_bar9">4回生</label>
                                 <ul id="link9">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 4)
@@ -189,8 +183,8 @@
                                     @endforeach
                                 </ul>
 
-                                <label  class="grade-female-label" for="cp_menu_bar10">3回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar10" class="accordion" />
+                                <label  class="grade-female-label" for="cp_menu_bar10">3回生</label>
                                 <ul id="link10">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 3)
@@ -206,8 +200,8 @@
                                     @endforeach
                                 </ul>
 
-                                <label  class="grade-female-label" for="cp_menu_bar11">2回生</label>
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar11" class="accordion" />
+                                <label  class="grade-female-label" for="cp_menu_bar11">2回生</label>
                                 <ul id="link11">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 2)
@@ -223,8 +217,9 @@
                                     @endforeach
                                 </ul>
 
-                                <label  class="grade-female-label" for="cp_menu_bar12">1回生</label>
+
                                 <input type="checkbox" name="checkbox" id="cp_menu_bar12" class="accordion" />
+                                <label  class="grade-female-label" for="cp_menu_bar12">1回生</label>
                                 <ul id="link12">
                                     @foreach($members as $member)
                                         @if($fiscal_year - $member->enrollment_year + 1 == 1)
