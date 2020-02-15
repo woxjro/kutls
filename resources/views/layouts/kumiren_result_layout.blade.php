549755c8a000c5a21ddac8bd5e9bf03c30af7ad2
YU-NN <yoshi_nn_ihsoy@icloud.com>
2020-02-16 03:38:24 +0900

    jqueryのversion変更

--- resources/views/layouts/kumiren_result_layout.blade.php
+++ resources/views/layouts/kumiren_result_layout.blade.php
@@ -24,7 +24,7 @@
 
 
     <!-- jQuery / jQuery UI -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 
     <!-- jQuery Touch Punch - Enable Touch Drag and Drop -->
--- resources/views/layouts/kumiren_staffs_layout.blade.php
+++ resources/views/layouts/kumiren_staffs_layout.blade.php
@@ -27,7 +27,7 @@
 
 
     <!-- jQuery / jQuery UI -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
